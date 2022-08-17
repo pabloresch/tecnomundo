4dc57423c51be32c63bdf6e5b6afa5efbddf5e4d
PeterSauma <pedrosaumacammi@gmail.com>
2022-08-17 01:42:12 -0300

    Se trabajó en validaciones.js para hacer las validaciones del Front en el formulario de Register y se modificó su html correspondiente en LogIn.ejs

--- public/js/validaciones.js
+++ public/js/validaciones.js
@@ -1,12 +1,15 @@
 window.addEventListener('load', function(){
-    let form = document.querySelector('#login')
-    let button = document.querySelector('.submit-login')
-    let nameError = document.querySelector('#nameError')
+  
+  /**********LogIn**********/
+
+    let formLogIn = document.querySelector('#login')
+    let buttonLogIn = document.querySelector('.submit-login')
+    let nameErrorLogIn = document.querySelector('#nameErrorLogIn')
     let correo = document.querySelector('#correo')
 
-    console.log(form.correo.value)
+    console.log(formLogIn.correo.value)
 
-  button.addEventListener('click', function(event) {
+  buttonLogIn.addEventListener('click', function(event) {
       event.preventDefault();
       let errores = {};
       
@@ -16,10 +19,55 @@ window.addEventListener('load', function(){
     }
 
     if(Object.keys(errores).length >= 1) {
-        nameError.innerHTML = (errores.correo) ? errores.correo : '';
+        nameErrorLogIn.innerHTML = (errores.correo) ? errores.correo : '';
 
     } else {
-        form.submit()
+        formLogIn.submit()
     }
+  })
+  /**********Register**********/
+  //Capturo el formulario de Registro completo
+  let formRegister = document.querySelector('#registro')
+  //Capturo todos los divs con text danger que van a aparecer cuando valide
+  let nameRgErr = document.querySelector('#nameRgErr')
+  let lastNameRgErr = document.querySelector('#lastNameRgErr')
+  let emailRgErr = document.querySelector('#emailRgErr')
+  let passwordRgErr = document.querySelector('#passwordRgErr')
+  let passconfconRgErr = document.querySelector('#passconfconRgErr')
+  
+  //Validacion del nombre. Este imput no debe contener menos de dos caracteres
+  formRegister.nameRg.addEventListener('blur', function(){
+    nameRgErr.innerHTML = 'El nombre debe tener 2 caracteres o más'
+  })
+  //Validacion del apellido. Este imput no debe contener menos de dos caracteres
+  formRegister.lastNameRg.addEventListener('blur', function(){
+   lastNameRgErr.innerHTML = 'El apellido debe tener 2 caracteres o más'
+  })
+  //Validacion del email. Debe ser un mail váilido, no debe repetirse en la database
+  formRegister.emailRg.addEventListener('blur', function(){
+   emailRgErr.innerHTML = 'El email debe ser válido.'
+   emailRgErr.innerHTML += ' Este email ya se encuentra registrado'
+  })
+  //Validacion del password. Debe tener 8 caracteres mínimos, debe tener mayusculas y minusculas, un número y un caracter especial
+  formRegister.passwordRg.addEventListener('focus', function(){
+    passwordRgErr.innerHTML = 'Debe tener 8 caracteres mínimos, debe tener mayusculas y minusculas, un número y un caracter especial'
+  })
+  //Validacion del passconfcon. Deben coincidir las contraseñas
+  formRegister.passconfconRg.addEventListener('focus', function(){
+    console.log(1);
+    passconfconRgErr.innerHTML = 'Las contraseñas deben coincidir'
+  })
+  //Validacion del avatar. Deberá ser un archivo válido (JPG, JPEG, PNG, GIF)
+  formRegister.avatar.addEventListener('blur', function(){
+    console.log(6);
+  })
+
+
+  //Validacion del formulario completo. Si surje un solo error el formulario no se envía
+  formRegister.addEventListener('submit', function(event){
+    event.preventDefault();
+    let errores = {};
+    return alert ('Sprint 7 allá vamos!!!')
+    
   })
 })
