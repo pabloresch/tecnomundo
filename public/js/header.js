d74ef4bf1a87b94e7c4653919f4579d4fba44937
pabloresch <pabloresch011@gmail.com>
2022-09-05 08:40:53 +1000

    header fix

--- public/js/header.js
+++ public/js/header.js
@@ -1,11 +1,9 @@
 window.addEventListener('load', function () {
   
-  
+  let contador = 1
   const cartIcon = () => {
         if (localStorage.carrito && localStorage.carrito != '{}') {
-            document.querySelector('#cart-icon').innerHTML='<i class="fa-solid fa-cart-plus"></i>'
-        } else {
-          document.querySelector('#cart-icon').innerHTML='<i class="fa-solid fa-cart-shopping"></i>'
+            document.querySelector('#cart-icon').innerHTML +=`<span class ="badge badge-danger badge-counter">${contador}</span>` 
         }
       }
     
